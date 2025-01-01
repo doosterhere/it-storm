bc16fcb3d9f680804eb980f09f81244aba164b6c
Mikalai Shcharbakou <doosterhere@gmail.com>
2025-01-01 13:50:16 +0300

    config: change paths for the production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: "https://good-red-dugong-wig.cyclic.app/api/",
-  serverStaticPath: "https://good-red-dugong-wig.cyclic.app/images/"
+  api: "https://it-storm-backend.vercel.app/api/",
+  serverStaticPath: "https://it-storm-backend.vercel.app/images/"
 }
